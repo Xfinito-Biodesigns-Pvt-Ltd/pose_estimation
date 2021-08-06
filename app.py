ea5ade8f604a3b11c7839cd82479e9d2b28e559c
sivarajendran <61067778+sivarajendran@users.noreply.github.com>
2021-08-06 16:25:14 +0530

    added new changes

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-# Library files:
+import cv2
 import mediapipe as mdp
 import math
 import numpy as np
 import time
-import streamlit as st
-import cv2
+import moviepy
 
-# Initial conditions:
+
+
+mdp_drawing = mdp.solutions.drawing_utils
+mdp_pose = mdp.solutions.pose
+
+# In[2]:
+
+
+vid_cap = cv2.VideoCapture('video5.mp4')
 prev_time = 0
 steps = 0
 step_left = 0
@@ -17,157 +24,163 @@ H = 0
 L1 = 0
 H1 = 0
 
+with mdp_pose.Pose(min_detection_confidence= 0.5, min_tracking_confidence=0.5) as pose:
+ while  vid_cap.isOpened():
+    ret, img = vid_cap.read()
 
-## For executing webapplication:
-def main():
-    """Pose estimation App"""
+    if ret:
+        col_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # converting BGR2GRAY for the sake of mediapipe
 
+        pose_sols = mdp.solutions.pose.Pose().process(col_img)  # mediapipe solutions for Pose detection
+        landmarks_info = pose_sols.pose_landmarks  # to understand the landmarks
 
-st.title("Pose estimator")
+        if landmarks_info:
+            mdp.solutions.drawing_utils.draw_landmarks(img, landmarks_info,
+                                                   mdp.solutions.pose.POSE_CONNECTIONS)
+        landmarks_info_list = []
+        for idx, landmarks in enumerate(landmarks_info.landmark):  # identifying
+            hgt, wdt, chnl = img.shape  # landmarks
+        # at the their
+            landmarks_info_list.append([idx, int(landmarks.x * wdt), int(landmarks.y * hgt)])
+            cv2.circle(img, (int(landmarks.x * wdt), int(landmarks.y * hgt)), 3, (0, 255, 0))  # positions with a circle
+        # print(landmarks_info_list)
 
-html_temp = """
-          <body style="background-color:red;">
-           <div style="background-color:teal ;padding:10px">
-           <h2 style="color:white;text-align:center;">Pose Estimation WebApp</h2>
-           </div>
-           </body>
-           """
-st.markdown(html_temp, unsafe_allow_html=True)
+    # lt_shoulder11 = [landmarks_info_list[11][1], landmarks_info_list[11][2]]
+    # lt_elbow13 = [landmarks_info_list[13][1], landmarks_info_list[13][2]]
+    # lt_wrist15 = [landmarks_info_list[15][1], landmarks_info_list[15][2]]
 
-video_file = st.file_uploader("Upload your video", type=['mp4', 'avi'])
+    # lt_hand_angle = abs(round(math.degrees(
+    # math.atan2(lt_wrist15[1] - lt_elbow13[1], lt_wrist15[0] - lt_elbow13[0]) - math.atan2(
+    # lt_shoulder11[1] - lt_elbow13[1], lt_shoulder11[0] - lt_elbow13[0]))))
 
+    # rt_shoulder12 = [landmarks_info_list[12][1], landmarks_info_list[12][2]]
+    # rt_elbow14 = [landmarks_info_list[14][1], landmarks_info_list[14][2]]
+    # rt_wrist16 = [landmarks_info_list[16][1], landmarks_info_list[16][2]]
 
-## Main logic algorithm:
-while True:
-     ret, img = video_file.read()
-     col_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # rt_hand_angle = abs(round(math.degrees(
+    # math.atan2(rt_wrist16[1] - rt_elbow14[1], rt_wrist16[0] - rt_elbow14[0]) - math.atan2(
+    # rt_shoulder12[1] - rt_elbow14[1], rt_shoulder12[0] - rt_elbow14[0]))))
 
-     pose_sols = mdp.solutions.pose.Pose().process(col_img)
-     landmarks_info = pose_sols.pose_landmarks
+        lt_hip23 = [landmarks_info_list[23][1], landmarks_info_list[23][2]]
+        lt_knee25 = [landmarks_info_list[25][1], landmarks_info_list[25][2]]
+        lt_ankle27 = [landmarks_info_list[27][1], landmarks_info_list[27][2]]
+        lt_heel29 = [landmarks_info_list[29][1], landmarks_info_list[29][2]]
+        lt_foot_index31 = [landmarks_info_list[31][1], landmarks_info_list[31][2]]
 
-     if landmarks_info:
-         mdp.solutions.drawing_utils.draw_landmarks(img, landmarks_info,
-                                                    mdp.solutions.pose.POSE_CONNECTIONS)
-     landmarks_info_list = []
-     for idx, landmarks in enumerate(landmarks_info.landmark):
-         hgt, wdt, chnl = img.shape
+        lt_leg_angle = abs(180 - round(math.degrees(
+        math.atan2(lt_ankle27[1] - lt_knee25[1], lt_ankle27[0] - lt_knee25[0]) - math.atan2(
+            lt_hip23[1] - lt_knee25[1],
+            lt_hip23[0] - lt_knee25[
+                0]))))
 
-         landmarks_info_list.append([idx, int(landmarks.x * wdt), int(landmarks.y * hgt)])
-         cv2.circle(img, (int(landmarks.x * wdt), int(landmarks.y * hgt)), 3, (0, 255, 0))
+        lt_ankle_angle = abs(360 - round(math.degrees(
+        math.atan2(lt_foot_index31[1] - lt_ankle27[1], lt_foot_index31[0] - lt_ankle27[0]) - math.atan2(
+            lt_knee25[1] - lt_ankle27[1],
+            lt_knee25[0] - lt_ankle27[0]))))
 
-     lt_hip23 = [landmarks_info_list[23][1], landmarks_info_list[23][2]]
-     lt_knee25 = [landmarks_info_list[25][1], landmarks_info_list[25][2]]
-     lt_ankle27 = [landmarks_info_list[27][1], landmarks_info_list[27][2]]
-     lt_heel29 = [landmarks_info_list[29][1], landmarks_info_list[29][2]]
-     lt_foot_index31 = [landmarks_info_list[31][1], landmarks_info_list[31][2]]
+        lt_angle = lt_ankle_angle
 
-     lt_leg_angle = abs(180 - round(math.degrees(
-         math.atan2(lt_ankle27[1] - lt_knee25[1], lt_ankle27[0] - lt_knee25[0]) - math.atan2(
-             lt_hip23[1] - lt_knee25[1],
-             lt_hip23[0] - lt_knee25[
-                 0]))))
+        rt_hip24 = [landmarks_info_list[24][1], landmarks_info_list[24][2]]
+        rt_knee26 = [landmarks_info_list[26][1], landmarks_info_list[26][2]]
+        rt_ankle28 = [landmarks_info_list[28][1], landmarks_info_list[28][2]]
+        rt_heel30 = [landmarks_info_list[30][1], landmarks_info_list[30][2]]
+        rt_foot_index32 = [landmarks_info_list[32][1], landmarks_info_list[32][2]]
 
-     lt_ankle_angle = abs(360 - round(math.degrees(
-         math.atan2(lt_foot_index31[1] - lt_ankle27[1], lt_foot_index31[0] - lt_ankle27[0]) - math.atan2(
-             lt_knee25[1] - lt_ankle27[1],
-             lt_knee25[0] - lt_ankle27[0]))))
+        rt_leg_angle = abs(180 - round(math.degrees(
+            math.atan2(rt_ankle28[1] - rt_knee26[1], rt_ankle28[0] - rt_knee26[0]) - math.atan2(
+            rt_hip24[1] - rt_knee26[1],
+            rt_hip24[0] - rt_knee26[
+                0]))))
 
-     lt_angle = lt_ankle_angle
+        if len(landmarks_info_list) != 0:
+            cv2.circle(img, (rt_hip24[0], rt_hip24[1]), 5, (255, 0, 0))
+            cv2.circle(img, (rt_knee26[0], rt_knee26[1]), 5, (255, 0, 0))
+            cv2.circle(img, (rt_ankle28[0], rt_ankle28[1]), 5, (255, 0, 0))
+            cv2.circle(img, (rt_heel30[0], rt_heel30[1]), 5, (255, 0, 0))
+            cv2.circle(img, (rt_foot_index32[0], rt_foot_index32[1]), 5, (255, 0, 0))
 
-     rt_hip24 = [landmarks_info_list[24][1], landmarks_info_list[24][2]]
-     rt_knee26 = [landmarks_info_list[26][1], landmarks_info_list[26][2]]
-     rt_ankle28 = [landmarks_info_list[28][1], landmarks_info_list[28][2]]
-     rt_heel30 = [landmarks_info_list[30][1], landmarks_info_list[30][2]]
-     rt_foot_index32 = [landmarks_info_list[32][1], landmarks_info_list[32][2]]
+            cv2.circle(img, (lt_hip23[0], lt_hip23[1]), 5, (255, 0, 0))
+            cv2.circle(img, (lt_knee25[0], lt_knee25[1]), 5, (255, 0, 0))
+            cv2.circle(img, (lt_ankle27[0], lt_ankle27[1]), 5, (255, 0, 0))
+            cv2.circle(img, (lt_heel29[0], lt_heel29[1]), 5, (255, 0, 0))
+            cv2.circle(img, (lt_foot_index31[0], lt_foot_index31[1]), 5, (255, 0, 0))
 
-     rt_leg_angle = abs(180 - round(math.degrees(
-         math.atan2(rt_ankle28[1] - rt_knee26[1], rt_ankle28[0] - rt_knee26[0]) - math.atan2(
-             rt_hip24[1] - rt_knee26[1],
-             rt_hip24[0] - rt_knee26[
-                 0]))))
+            #cv2.putText(img, str(int(lt_leg_angle)), (lt_knee25[0] - 20, lt_knee25[1] + 20), cv2.FONT_HERSHEY_PLAIN,
+                    #1.5,
+                    #(0, 0, 255), 2)
+        # cv2.putText(img, str(int(lt_angle)), (lt_ankle27[0] - 20, lt_ankle27[1] + 20), cv2.FONT_HERSHEY_PLAIN,
+        #           1.5,
+        #          (0, 0, 255), 2)
+        # cv2.putText(img, str(int(rt_leg_angle)), (rt_knee26[0] - 20, rt_knee26[1] + 20), cv2.FONT_HERSHEY_PLAIN,
+        #  1.5,
+        # (0, 0, 255), 2)
 
-     if len(landmarks_info_list) != 0:
-         cv2.circle(img, (rt_hip24[0], rt_hip24[1]), 5, (255, 0, 0))
-         cv2.circle(img, (rt_knee26[0], rt_knee26[1]), 5, (255, 0, 0))
-         cv2.circle(img, (rt_ankle28[0], rt_ankle28[1]), 5, (255, 0, 0))
-         cv2.circle(img, (rt_heel30[0], rt_heel30[1]), 5, (255, 0, 0))
-         cv2.circle(img, (rt_foot_index32[0], rt_foot_index32[1]), 5, (255, 0, 0))
+        if (lt_leg_angle <= 15):
+            L = 0
+            H = 0
 
-         cv2.circle(img, (lt_hip23[0], lt_hip23[1]), 5, (255, 0, 0))
-         cv2.circle(img, (lt_knee25[0], lt_knee25[1]), 5, (255, 0, 0))
-         cv2.circle(img, (lt_ankle27[0], lt_ankle27[1]), 5, (255, 0, 0))
-         cv2.circle(img, (lt_heel29[0], lt_heel29[1]), 5, (255, 0, 0))
-         cv2.circle(img, (lt_foot_index31[0], lt_foot_index31[1]), 5, (255, 0, 0))
+        if (lt_leg_angle >= 15 and H == 0):
+            L = 1
 
-         cv2.putText(img, str(int(lt_leg_angle)), (lt_knee25[0] - 20, lt_knee25[1] + 20), cv2.FONT_HERSHEY_PLAIN,
-                     1.5,
-                     (0, 0, 255), 2)
+        else:
+            L = 0
 
-         cv2.putText(img, str(int(rt_leg_angle)), (rt_knee26[0] - 20, rt_knee26[1] + 20), cv2.FONT_HERSHEY_PLAIN,
-                     1.5,
-                     (0, 0, 255), 2)
+        if lt_leg_angle >= 50 and L == 1:
+            H = 1
 
-     if (lt_leg_angle <= 15):
-         L = 0
-         H = 0
+        D = [L == 1 and H == 1]
+        if True in D:
+            step_left += 1
+        # print(f'step_count', {step_left})
+        # print(L, H)
 
-     if (lt_leg_angle >= 15 and H == 0):
-         L = 1
+        L = 0
 
-     else:
-         L = 0
+    # Right leg angle:
+        if (rt_leg_angle <= 10):
+            L1 = 0
+            H1 = 0
 
-     if lt_leg_angle >= 50 and L == 1:
-         H = 1
+        if (rt_leg_angle >= 10 and H1 == 0):
+            L1 = 1
 
-     D = [L == 1 and H == 1]
-     if True in D:
-         step_left += 1
+        else:
+            L1 = 0
 
-     L = 0
+        if rt_leg_angle >= 35 and L1 == 1:
+            H1 = 1
 
-     if (rt_leg_angle <= 15):
-         L1 = 0
-         H1 = 0
+        # print((rt_leg_angle))
 
-     if (rt_leg_angle >= 15 and H1 == 0):
-         L1 = 1
+        K = [L1 == 1 and H1 == 1]
+        if True in K:
+            step_right += 1
+        # print(f'step_count_right', {step_right})
+        # print(L, H)
 
-     else:
-         L1 = 0
+        L1 = 0
 
-     if rt_leg_angle >= 45 and L1 == 1:
-         H1 = 1
+        if True in K or True in D:
+            total_steps = step_left + step_right
 
-     K = [L1 == 1 and H1 == 1]
-     if True in K:
-         step_right += 1
+            print(f'Step no:', {total_steps})
 
-     L1 = 0
+        cur_time = time.time()
+        fps = 1 / (cur_time - prev_time)
+        prev_time = cur_time
 
-     if True in K or True in D:
-         total_steps = step_left + step_right
+        #cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 1.5, (255, 0, 0), 2)
 
-         print(f'Step no:', {total_steps})
+        cv2.imshow("Video Frame", img)
 
-         # arr = []
-         # for i in range(0, total_steps):
-         # arr.append(total_steps)
-         # print(arr)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    else:
+        break
 
-     #cur_time = time.time()
-     #fps = 1 / (cur_time - prev_time)
-     #prev_time = cur_time
+ vid_cap.release()
+ cv2.destroyAllWindows()
 
-     #cv2.imshow("Pose estimator", img)
-
-
-#st.video(img)
-st.text(total_steps)
-     
-if __name__ == '__main__':
-        main()
-         
 
 
 
